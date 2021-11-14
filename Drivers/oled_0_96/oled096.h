bf1ab69ca0843ee1605e2a753b5b22a89536390c
Null <37436671+Nulliik@users.noreply.github.com>
2021-11-14 22:53:56 +0300

    Formated header files
    Removes unused spaces, added a bit more comms

--- Drivers/oled_0_96/oled096.h
+++ Drivers/oled_0_96/oled096.h
@@ -38,70 +38,49 @@
 #define OLED_ADRESS 0x78
 #define OLED_MAX_TRIALS 1
 
-
-
 /* USER CODE BEGIN PV */
 /* Imported variables ---------------------------------------------------------*/
 
-
-/* private variables ---------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
 
 /* USER CODE END PV */
-
-
 
 /* Private types -------------------------------------------------------------*/
 
 /**
-  * @brief  OLED communication statuses
-  */
-typedef enum
-{
-	OLED_OK       = 0x00U,
-	OLED_ERROR    = 0x01U,
-	OLED_BUSY     = 0x02U,
-	OLED_TIMEOUT  = 0x03U,
+ * @brief  OLED communication statuses
+ */
+typedef enum {
+	OLED_OK = 0x00U,
+	OLED_ERROR = 0x01U,
+	OLED_BUSY = 0x02U,
+	OLED_TIMEOUT = 0x03U,
 	OLED_DESCFAIL = 0x04U
 } OLED_StatusTypeDef;
-/**
-  *  END
-  */
-
-
 
 /**
-  * @brief  OLED Error Management
-  */
-typedef enum
-{
-	HANDLE_SUCCESS		= 0x00U,
-	HANDLE_FAULT 		= 0x01U
-}OLED_ErrorHandlerType;
-/**
-  *  END
-  */
+ * @brief  OLED Error Management
+ */
+typedef enum {
+	HANDLE_SUCCESS = 0x00U,
+	HANDLE_FAULT = 0x01U
+} OLED_ErrorHandlerType;
 
-
-
-typedef enum
-{
-	COMMAND		  = 0x00U,
-	DATA    	  = 0x40U
+typedef enum {
+	COMMAND = 0x00U,
+	DATA = 0x40U
 } OLED_DataType;
 
 /**
-  * @brief  OLED data transmit function type
-  */
-typedef OLED_StatusTypeDef (*DataSend)(OLED_DataType Descriptor, uint8_t AddressI2C, uint8_t *Data, size_t length);
-/**
-  *  END
-  */
+ * @brief  OLED data transmit function type
+ */
+typedef OLED_StatusTypeDef (*DataSend)(OLED_DataType Descriptor,
+		uint8_t AddressI2C, uint8_t *Data, size_t length);
 
 /**
-  * @brief  OLED Cursor current position
-  */
-typedef struct
-{
+ * @brief  OLED Cursor current position
+ */
+typedef struct {
 	uint8_t Page;
 
 	uint8_t Segment;
@@ -109,70 +88,44 @@ typedef struct
 } OLED_CursorCoordTypeDef;
 
 /**
-  * @brief  OLED main structure
-  */
-typedef struct
-{
-	uint8_t AddressI2C; 				// Can be 111100 or 111101, default 111100(for STM HAL left shift needed)
+ * @brief  OLED main structure
+ */
+typedef struct {
+	uint8_t AddressI2C; // Can be 111100 or 111101, default 111100(for STM HAL left shift needed)
 
 	DataSend DataSend;					// Abstract data send function
 
 	uint8_t OLEDErrorSolvingTrials;		// Amount of tries to reach OLED
 
-	uint8_t * FrameMem;					// Pointer to heap area for frame buffer
+	uint8_t *FrameMem;					// Pointer to heap area for frame buffer
 
-	uint8_t Width;						// Definition of display in 8 bit segments
+	uint8_t Width;					// Definition of display in 8 bit segments
 
 	uint8_t Heigth;						// Definition of display in pages
 
 	uint16_t FrameSize;					// Pages X Segments - in bytes
 
-	OLED_CursorCoordTypeDef * Cursor;	// Active segment and page in GDDR for new data
+	OLED_CursorCoordTypeDef *Cursor;// Active segment and page in GDDR for new data
 
-}OLED_HandleTypeDef;
-
-
-
-
-
-
-
-
+} OLED_HandleTypeDef;
 
 /**
-  * @brief  HAL Lock structures definition
-  */
-typedef enum
-{
-  OLED_UNLOCKED = 0x00U,
-  OLED_LOCKED   = 0x01U
+ * @brief  HAL Lock structures definition
+ */
+typedef enum {
+	OLED_UNLOCKED = 0x00U,
+	OLED_LOCKED = 0x01U
 } OLED_LockTypeDef;
-
-
 
 extern OLED_HandleTypeDef OLED096;
 
-
 /* Private function prototypes -----------------------------------------------*/
-/* OLED FUNCTIONS BEGIN PROTOTYPES */
 
+/* OLED FUNCTIONS BEGIN PROTOTYPES */
 
 OLED_StatusTypeDef OLED_Init(OLED_HandleTypeDef *OLED);
 OLED_StatusTypeDef OLED_DeInit(OLED_HandleTypeDef *OLED);
-OLED_StatusTypeDef OLED_FrameRefresh (OLED_HandleTypeDef *OLED);
+OLED_StatusTypeDef OLED_FrameRefresh(OLED_HandleTypeDef *OLED);
 OLED_StatusTypeDef OLED_DrawTestImage(OLED_HandleTypeDef *OLED);
 
-
-
-
-void LCD_Char(uint8_t c);
-
-void OLED_Set_Cursor(OLED_HandleTypeDef* OLED, uint8_t Byte, uint8_t Page);
-
-void OLED_string(char *string);
-void OLED_num_to_str(unsigned int value, unsigned char nDigit);
 /* OLED FUNCTIONS END PROTOTYPES */
-
-/* OLED Function refactored */
-
-
